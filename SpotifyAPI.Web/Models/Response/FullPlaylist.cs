c6edcd72f7c44e5ee056e1c947ff1c81812dc70b
Quin Lynch <quin@edgedb.com>
2024-04-21 21:30:53 -0300

    change nullables

--- SpotifyAPI.Web/Models/Response/FullPlaylist.cs
+++ SpotifyAPI.Web/Models/Response/FullPlaylist.cs
@@ -6,7 +6,7 @@ namespace SpotifyAPI.Web
     public bool? Collaborative { get; set; }
     public string? Description { get; set; } = default!;
     public Dictionary<string, string>? ExternalUrls { get; set; } = default!;
-    public Followers Followers { get; set; } = default!;
+    public Followers? Followers { get; set; } = default!;
     public string? Href { get; set; } = default!;
     public string? Id { get; set; } = default!;
     public List<Image>? Images { get; set; } = default!;
